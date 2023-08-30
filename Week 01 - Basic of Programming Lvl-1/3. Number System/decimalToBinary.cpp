3135563634272b22c9d5ff99dc6ce67a1bfa8f83
Saurabh <saurabhmahapatra677@gmail.com>
2023-08-31 00:51:27 +0530

    Decimal To Binary code of Number System added

---
+++ Week 01 - Basic of Programming Lvl-1/3. Number System/decimalToBinary.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+#include<cmath>
+using namespace std;
+
+
+//By Using Division Method
+int decimalToBinary(int n){
+	int binaryNo =0;
+	int i = 0;
+	while(n>0){
+		int bit = n % 2;
+		binaryNo = bit*pow(10, i++) + binaryNo;
+		n = n / 2;
+	}
+	return binaryNo;
+}
+
+int main()
+{
+    int n;
+    cin>>n;
+    int binary = decimalToBinary(n);
+    cout<<binary;	
+    return 0;
+}
