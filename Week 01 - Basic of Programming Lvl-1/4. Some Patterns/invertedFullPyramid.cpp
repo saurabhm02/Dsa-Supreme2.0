215fe04bce98fecfb65ccfa471cd4ae96f5731a7
Saurabh <saurabhmahapatra677@gmail.com>
2023-09-10 22:18:37 +0530

    Inverted Full-Pyramid Pattern added

---
+++ Week 01 - Basic of Programming Lvl-1/4. Some Patterns/invertedFullPyramid.cpp
@@ -0,0 +1,30 @@
+
+// * * * * * 
+//  * * * * 
+//   * * * 
+//    * * 
+//     *
+
+#include<iostream>
+using namespace std;
+#define ll long long
+
+void invertedFullPyramid(int size){
+	for(int i=0; i<size; i++){
+		for(int j=0; j<i; j++){
+			cout<<" ";
+		}
+		for(int j=0; j<size-i; j++){
+			cout<<"* ";
+		}
+		cout<<endl;
+	}
+}
+int main()
+{
+	int size;
+	cin>>size;
+	invertedFullPyramid(size);
+	
+    return 0;
+}
