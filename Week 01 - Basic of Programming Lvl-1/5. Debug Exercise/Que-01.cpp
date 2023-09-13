992f60534fc5baad8efecebd5552b3f5b0576b47
Saurabh Mahapatra <138717117+saurabhm02@users.noreply.github.com>
2023-09-14 02:43:44 +0530

    Update Que-01.cpp
    Que 1 of debug Exercise

--- Week 01 - Basic of Programming Lvl-1/5. Debug Exercise/Que-01.cpp
+++ Week 01 - Basic of Programming Lvl-1/5. Debug Exercise/Que-01.cpp
@@ -1,6 +1,7 @@
 
 // Que. Debug the code. Take input a character, print 1, 
 // if its a capital alphabet, print 0, if its a lowercase alphabet, else print -1.
+// here the ans 
 
 
 #include<iostream>
