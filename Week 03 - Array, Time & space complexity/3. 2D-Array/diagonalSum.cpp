ecd541508161b6f5337a0affb5f9999a1b354f0c
Saurabh <saurabhmahapatra677@gmail.com>
2023-09-18 09:51:50 +0530

    Diagonal-Sum in 2-D Array added

---
+++ Week 03 - Array, Time & space complexity/3. 2D-Array/diagonalSum.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+#include<vector>
+
+using namespace std;
+#define ll long long
+
+int diagonalSum(vector<vector<int>> &arr, int rows, int column){
+	int sum = 0;
+	for(int i=0;i<rows; i++){
+		sum = sum + arr[i][i];
+	}
+	return sum;
+}
+int main()
+{
+	int rows, columns;
+	cin>>rows>>columns;
+	vector<vector<int>> arr(rows, vector<int>(columns));
+	for(int i=0;i<rows; i++){
+		for(int j=0; j<columns;j++){
+			cin>>arr[i][j];
+		}
+	}
+
+cout<<diagonalSum(arr, rows, columns);
+	
+
+    return 0;
+}
