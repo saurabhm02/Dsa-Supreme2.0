0e669872e85723a47cb4f3c4cbacb6d3e1e246f5
Saurabh <saurabhmahapatra677@gmail.com>
2023-09-24 20:46:31 +0530

    Last Occurence in Array problem solution

---
+++ Week 04 - Searching && Sorting/1. Searching Lvl-01/lastOccurence.cpp
@@ -0,0 +1,45 @@
+#include<iostream>
+#include<vector>
+
+using namespace std;
+#define ll long long
+
+int lastOccurence(vector<int>arr, int size, int target){
+	int start = 0;
+	int end = size-1;
+	int mid = start + (end - start) / 2;
+	int ans = -1;	 
+    while(start <= end){
+    	
+
+    	if(arr[mid] == target){
+			ans = mid;
+			start = mid + 1;
+		} 
+		else if(target < arr[mid]){
+			end = mid - 1;
+		}
+		else if(target > arr[mid]){
+			start = mid + 1;
+		}
+
+		mid = start + (end - start) / 2;
+    }
+	return ans;
+}
+int main()
+{
+
+	int size;
+	cin>>size;
+	vector<int>arr(size);
+	for(int i=0; i<size; i++){
+		cin>>arr[i];
+	}
+	int target;
+	cin>>target;
+
+	cout<<lastOccurence(arr, size, target);
+	
+    return 0;
+}
