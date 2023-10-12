61036e41ce490924f4079371c8b523c0a4b028a3
Saurabh Mahapatra <saurabhmahapatra677@gmail.com>
2023-10-13 00:34:34 +0530

    Que 01 Debug Exercise Solution added

---
+++ Week 02 - Basic of Programminf Lvl-2/3. Debug Exercise/Que 01.cpp
@@ -0,0 +1,28 @@
+// Que . 1.Debug the code. This code is trying to calculate area of a rectangle.
+
+#include<iostream>
+#include<vector>
+
+using namespace std;
+#define ll long long
+
+
+// Before Debug 
+// int main()
+// {
+// 	double width, height;
+// 	cin >> width >> height;
+// 	int area = width * height;
+// 	cout << area << endl;
+//     return 0;
+// }
+
+// After Debug
+int main()
+{
+	double width, height;
+	cin >> width >> height;
+	double area = width * height;
+	cout << area << endl;
+    return 0;
+}
