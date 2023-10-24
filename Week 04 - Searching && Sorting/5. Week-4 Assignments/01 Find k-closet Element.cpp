934b32c4200750f1f330b79def1a85e715afcee4
Saurabh Mahapatra <saurabhmahapatra677@gmail.com>
2023-10-24 22:18:50 +0530

    Solution of K-Closet Element added

---
+++ Week 04 - Searching && Sorting/5. Week-4 Assignments/01 Find k-closet Element.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+#include<vector>
+
+using namespace std;
+#define ll long long
+
+int main()
+{
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif 
+
+int n;
+cin>>n;
+cout<<n;
+    return 0;
+}
