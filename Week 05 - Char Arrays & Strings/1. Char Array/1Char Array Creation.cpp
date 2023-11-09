c7c66949c0b37c24b885b21494515f3192cd22e4
Saurabh Mahapatra <saurabhmahapatra677@gmail.com>
2023-11-09 21:10:15 +0530

    updated

--- Week 05 - Char Arrays & Strings/1. Char Array/1Char Array Creation.cpp
+++ Week 05 - Char Arrays & Strings/1. Char Array/1Char Array Creation.cpp
@@ -1,90 +1,61 @@
-// #include<bits/stdc++.h>
+#include<bits/stdc++.h>
 
-// using namespace std;
-// #define ll long long
-
-// int findLength(char ch[], int size){
-//     // int length = 0;
-//     // for(int i=0; i<size; i++){
-//     //     if(ch[i]== '\0'){
-//     //         break;
-//     //     }
-//     //     else{
-//     //         length++;
-//     //     }
-//     // }
-//     // return length;
-//     int len = 0;
-//     while(ch[len] != '\0'){
-//         len++;
-//     }
-//     return len;
-// }
-// int main()
-// {
-// #ifndef ONLINE_JUDGE
-//     freopen("input.txt", "r", stdin);
-//     freopen("output.txt", "w", stdout);
-// #endif 
-
-//     // creation of character array
-//     char ch[50];
-//     // input
-//     // cin>>ch;
-//     // we can use for loop for taking input
-
-
-//     // output in char array
-//     // cout<<ch<<endl;
-//     // for(int i=0; i<10; i++){
-//     // 	cout<<ch[i];
-//     // }
-
-
-//     // for(int i=0; i<10; i++){
-//     // 	cout<<i<<" " <<ch[i]<<endl;
-//     // }
-
-//     // Use of getline
-   
-//     cin.getline(ch, 50);
-//     cout<<ch<<endl;
-//     cout<<sizeof(ch)<<endl;
-
-//     // find length untill get the null character "\0"
-//     cout<<findLength(ch, 100)<<endl;
-
-//     // inBuilt fun for lenght
-//     cout<<strlen(ch)<<endl;
-//     return 0;
-// }
-
-#include <iostream>
 using namespace std;
+#define ll long long
 
-bool isSquareFree(int n) {
-  for (int i = 2; i * i <= n; i++) {
-    if (n % (i * i) == 0) {
-      return false;
+int findLength(char ch[], int size){
+    // int length = 0;
+    // for(int i=0; i<size; i++){
+    //     if(ch[i]== '\0'){
+    //         break;
+    //     }
+    //     else{
+    //         length++;
+    //     }
+    // }
+    // return length;
+    int len = 0;
+    while(ch[len] != '\0'){
+        len++;
     }
-  }
-  return true;
+    return len;
+}
+int main()
+{
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif 
+
+    // creation of character array
+    char ch[50];
+    // input
+    // cin>>ch;
+    // we can use for loop for taking input
+
+
+    // output in char array
+    // cout<<ch<<endl;
+    // for(int i=0; i<10; i++){
+    // 	cout<<ch[i];
+    // }
+
+
+    // for(int i=0; i<10; i++){
+    // 	cout<<i<<" " <<ch[i]<<endl;
+    // }
+
+    // Use of getline
+   
+    cin.getline(ch, 50);
+    cout<<ch<<endl;
+    cout<<sizeof(ch)<<endl;
+
+    // find length untill get the null character "\0"
+    cout<<findLength(ch, 100)<<endl;
+
+    // inBuilt fun for lenght
+    cout<<strlen(ch)<<endl;
+    return 0;
 }
 
-int countSquareFreeDivisors(int n) {
-  int count = 0;
-  for (int i = 2; i <= n / 2; i++) {
-    if (n % i == 0 && isSquareFree(i)) {
-      count++;
-    }
-  }
-  return count;
-}
-
-int main() {
-  int n;
-  cin >> n;
-  int count = countSquareFreeDivisors(n);
-  cout << count << endl;
-  return 0;
-}
